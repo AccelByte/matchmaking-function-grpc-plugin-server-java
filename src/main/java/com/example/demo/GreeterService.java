e4750ce2eb8bf720bc8d4731616c072941bf5b4a
Ken Wang <ken.wang@accelbyte.net>
2022-07-25 09:47:12 -0700

    init commit

---
+++ src/main/java/com/example/demo/GreeterService.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.stub.StreamObserver;
+import org.lognet.springboot.grpc.GRpcService;
+
+@GRpcService
+public class GreeterService extends GreeterGrpc.GreeterImplBase {
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hellooooooo " + request.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
