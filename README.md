a99d169038d974dac4064403dc26289a743062bf
Tony Fu <tony.fu@accelbyte.net>
2022-08-31 15:17:37 -0700

    Added otel support for Graphana Cloud and Honeycomb, added enabled flag for interceptor, refactored logging

--- README.md
+++ README.md
@@ -10,14 +10,28 @@ docker buildx build -t mmgrpcserver-springboot --load .
 docker buildx rm mybuilder
 ```
 
-## Run docker image version locally
+
+## To run the dependencies only
+Copy .env.local to a new file called .env and get credentials from LassPass.
 ```
-docker run --name  mmgrpcserver-springboot \
+docker-compose -f docker-compose-dependencies.yaml up
+```
+
+## To run docker image version locally with Auth interceptor disabled
+```
+docker run --rm --log-driver=fluentd --log-opt tag=docker --log-opt fluentd-address=localhost:24225 --name  mmgrpcserver-springboot \
 -p6565:6565 -p8080:8080 \
--eJAVA_OPTS=-javaagent:aws-opentelemetry-agent.jar \
--eOTEL_EXPORTER_OTLP_ENDPOINT=http://host.docker.internal:4317/ \
+-eJAVA_OPTS='-javaagent:aws-opentelemetry-agent.jar -Djustice.grpc.interceptor.auth.enabled=false' \
+-eOTEL_EXPORTER_ZIPKIN_ENDPOINT=http://host.docker.internal:9411/api/v2/spans \
+-eOTEL_TRACES_EXPORTER=zipkin \
 -eOTEL_METRICS_EXPORTER=none \
 -eOTEL_SERVICE_NAME=CustomMatchMakingFunctionJavaDocker \
 -eOTEL_PROPAGATORS=b3multi \
 mmgrpcserver-springboot
+
+```
+
+## To run the app with its dependencies in one docker-compose command
+```
+docker-compose -f docker-compose-dependencies.yaml -f docker-compose-app.yaml up
 ```
---
+++ src/main/java/net/accelbyte/grpc/DebugLoggerServerInterceptor.java
@@ -0,0 +1,89 @@
+package net.accelbyte.grpc;
+
+import io.grpc.ForwardingServerCall.SimpleForwardingServerCall;
+import io.grpc.ForwardingServerCallListener;
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.ServerInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.lognet.springboot.grpc.GRpcGlobalInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+
+@Slf4j
+@GRpcGlobalInterceptor
+public class DebugLoggerServerInterceptor implements ServerInterceptor {
+    private static final String REQUEST = "REQUEST";
+    private static final String RESPONSE = "RESPONSE";
+
+    @Value("${justice.grpc.interceptor.debug-logger.enabled:false}")
+    private boolean enabled;
+
+    public DebugLoggerServerInterceptor() {
+        log.info("LoggingServerInterceptor initialized");
+    }
+
+    @Override
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers,
+                                                                 ServerCallHandler<ReqT, RespT> next) {
+        if(enabled) {
+            logMethod(REQUEST, call.getMethodDescriptor());
+            logHeaders(REQUEST, headers);
+        }
+        return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(
+                next.startCall(new SimpleForwardingServerCall<ReqT, RespT>(call) {
+
+                    @Override
+                    public void sendHeaders(Metadata responseHeaders) {
+                        if(enabled) {
+                            logMethod(RESPONSE, call.getMethodDescriptor());
+                            logHeaders(RESPONSE, responseHeaders);
+                        }
+                        super.sendHeaders(responseHeaders);
+                    }
+
+                    @Override
+                    public void sendMessage(RespT message) {
+                        if(enabled) {
+                            logMessage(RESPONSE, message);
+                        }
+                        super.sendMessage(message);
+                    }
+                }, headers)) {
+
+            @Override
+            public void onMessage(ReqT message) {
+                if(enabled) {
+                    logMessage(REQUEST, message);
+                }
+                super.onMessage(message);
+            }
+
+            @Override
+            public void onCancel() {
+                if(enabled) logCancellation(call.getMethodDescriptor());
+                super.onCancel();
+
+            }
+
+        };
+    }
+
+    private <ReqT, RespT> void logMethod(String type, MethodDescriptor<ReqT, RespT> method) {
+        log.info("{} PATH: {}", type, method.getFullMethodName());
+    }
+
+    private void logHeaders(String type, Metadata headers) {
+        log.info("{} HEADERS: {}}", type, headers);
+    }
+
+    private <T> void logMessage(String type, T message) {
+        log.info("{} MESSAGE: {}", type, message);
+    }
+
+    private <ReqT, RespT> void logCancellation(MethodDescriptor<ReqT, RespT> method) {
+        log.info("CALL CANCELLED for method {}", method.getFullMethodName());
+    }
+
+}
