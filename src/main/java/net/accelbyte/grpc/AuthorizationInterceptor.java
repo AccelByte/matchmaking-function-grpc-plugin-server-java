6a28238fe79a9c397be9135b287f95bc24cbf5a1
Ken Wang <ken.wang@accelbyte.net>
2022-09-19 20:29:59 -0700

    finished auth and exception handling

--- src/main/java/net/accelbyte/grpc/AuthorizationInterceptor.java
+++ src/main/java/net/accelbyte/grpc/AuthorizationInterceptor.java
@@ -4,10 +4,12 @@ import io.grpc.Metadata;
 import io.grpc.ServerCall;
 import io.grpc.ServerCallHandler;
 import io.grpc.ServerInterceptor;
+import io.grpc.Status;
 import lombok.extern.slf4j.Slf4j;
 import net.accelbyte.platform.exception.TokenIsExpiredException;
 import net.accelbyte.platform.exception.UnauthorizedException;
 import net.accelbyte.platform.security.OAuthToken;
+import net.accelbyte.platform.security.Permission;
 import net.accelbyte.platform.security.service.OAuthService;
 import org.lognet.springboot.grpc.GRpcGlobalInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,38 +18,54 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Slf4j
 @GRpcGlobalInterceptor
-
+@Order(20)
 public class AuthorizationInterceptor implements ServerInterceptor {
 
     @Value("${justice.grpc.interceptor.auth.enabled:true}")
     private boolean enabled;
+
+    private String namespace;
+
     private OAuthService oAuthService;
 
+    private Permission requiredPermission;
+
     @Autowired
-    public AuthorizationInterceptor(OAuthService oAuthService) {
+    public AuthorizationInterceptor(OAuthService oAuthService, @Value("${app.config.resource_name}") String resourceName, @Value("${app.config.namespace}") String namespace) {
         this.oAuthService = oAuthService;
+        this.requiredPermission = new Permission("NAMESPACE:" + namespace + ":" + resourceName, 2);
+        this.namespace = namespace;
         log.info("AuthorizationInterceptor initialized");
     }
 
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
         if(enabled) {
-            final String auth_token = headers.get(Metadata.Key.of("auth_token", Metadata.ASCII_STRING_MARSHALLER));
+            final String auth_token = headers.get(Metadata.Key.of("authorization", Metadata.ASCII_STRING_MARSHALLER));
 
             if (auth_token != null) {
                 try {
-                    OAuthToken oAuthToken = oAuthService.getOAuthToken(auth_token);
+                    String token = auth_token.split(" ")[1];
+                    OAuthToken oAuthToken = oAuthService.getOAuthToken(token);
+                    if(!oAuthService.validateTokenPermission(oAuthToken, requiredPermission, namespace, null)) {
+                        unAuthorizedCall(call, headers);
+                    }
                 } catch (TokenIsExpiredException e) {
                     log.warn("Caught TokenIsExpiredException, throw unauthorized exception");
-                    throw new UnauthorizedException();
+                    unAuthorizedCall(call, headers);
                 }
             } else {
-                throw new UnauthorizedException();
+                unAuthorizedCall(call, headers);
             }
         }
-        return next.startCall(call, headers);
+         return next.startCall(call, headers);
+    }
+
+    private <ReqT, RespT> void unAuthorizedCall(ServerCall<ReqT, RespT> call, Metadata headers) {
+        call.close(Status.UNAUTHENTICATED.withDescription("call not authorized"), headers);
     }
 }
